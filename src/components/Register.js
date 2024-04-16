e3be9447d8fa88e0c316c246fe80a54d12b59126
Biswarup Chatterjee <chatterjeebiswarup61@gmail.com>
2024-04-16 22:11:27 +0530

    work started on client side validation

--- src/components/Register.js
+++ src/components/Register.js
@@ -10,6 +10,32 @@ export default function Register() {
     const [role, setRole] = useState('')
     const [serverErrors, setServerErrors] = useState(null)
 
+    // create a state variable
+    const [clientErrors, setClientErrors] = useState(null)
+    //create a local variable
+    const errors = {}
+
+    const runValidation = () => {
+        if (username.trim().length === 0) {
+            errors.username = 'username is required'
+        }
+
+        if (email.trim().length === 0) {
+            errors.email = 'email is required'
+        }
+
+        if (password.trim().length === 0) {
+            errors.password = 'password is required'
+        } else if (
+            password.trim().length < 8 || password.trim().length > 128) {
+            errors.password = 'password should be between 8-128 characters'
+        }
+        if (role.trim().length === 0) {
+            errors.role = 'role is required'
+        }
+    }
+
+
     const handleSubmit = async (e) => {
         e.preventDefault()
         const formData = {
@@ -18,14 +44,23 @@ export default function Register() {
             password: password,
             role: role
         }
-        // TODO - client side validation
-        try {
-            const response = await axios.post('http://localhost:3333/users/register', formData)
-            console.log(response.data)
-            navigate('/login')
-        } catch (err) {
-            setServerErrors(err.response.data.errors)
+
+        runValidation()
+
+        if (Object.keys(errors).length === 0) {
+            try {
+                const response = await axios.post('http://localhost:3333/users/register', formData)
+                console.log(response.data)
+                navigate('/login')
+            } catch (err) {
+                setServerErrors(err.response.data.errors)
+            }
+        } else {
+            setClientErrors(errors)
         }
+
+
+
     }
 
     return (
@@ -55,6 +90,7 @@ export default function Register() {
                     }}
                     id="username"
                 />
+                {clientErrors && clientErrors.username && <span>{clientErrors.username}</span>}
                 <br />
                 <label htmlFor="email">Enter Email</label> <br />
                 <input
@@ -65,6 +101,8 @@ export default function Register() {
                     }}
                     id="email"
                 />
+                {clientErrors && clientErrors.email && <span>{clientErrors.email}</span>}
+
                 <br />
                 <label htmlFor="password">Enter Password</label> <br />
                 <input
@@ -75,6 +113,8 @@ export default function Register() {
                     }}
                     id="password"
                 />
+                {clientErrors && clientErrors.password && <span>{clientErrors.password}</span>}
+
                 <br />
                 <label htmlFor="">Select Role</label> <br />
                 <input
@@ -98,7 +138,9 @@ export default function Register() {
                     id="recruiter"
                     name="role"
                 />
-                <label htmlFor="recruiter">Recruiter</label>
+                <label htmlFor="recruiter">Recruiter</label> {' '}
+                {clientErrors && clientErrors.role && <span>{clientErrors.role}</span>}
+
                 <br />
                 <input type="submit" />
             </form>
